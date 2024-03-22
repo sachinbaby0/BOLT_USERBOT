da3c355b1257fc232ead755e33055ac5ae132930
SAGE_XD <159287148+Sagexdd@users.noreply.github.com>
2024-03-22 15:52:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,19 +6,19 @@ if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 
-API_ID = int(getenv("API_ID", "6435225")) #optional
+API_ID = int(getenv("API_ID", "25051663")) #optional
 API_HASH = getenv("API_HASH", "") #optional
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 OWNER_ID = int(getenv("OWNER_ID"))
 MONGO_URL = getenv("MONGO_URL")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-ALIVE_PIC = getenv("ALIVE_PIC", 'https://graph.org/file/c6a2ed96648fd03377dc9.jpg')
+ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph/file/6e8bb5bb7cd3a4a534683.jpg')
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
-REPO_URL = getenv("REPO_URL", "https://github.com/itzshukla/STRANGER-USERBOT2.0")
+REPO_URL = getenv("REPO_URL", "https://github.com/Sagexdd/USERBOT2")
 BRANCH = getenv("BRANCH", "master") #don't change
  
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
