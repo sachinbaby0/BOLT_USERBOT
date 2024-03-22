36aef328d6e52e63ec52c35821a911d1c11533c3
SAGE_XD <159287148+Sagexdd@users.noreply.github.com>
2024-03-22 14:10:11 +0530

    Update __init__.py

--- Zaid/helper/__init__.py
+++ Zaid/helper/__init__.py
@@ -9,6 +9,6 @@ def restart():
 
 async def join(client):
     try:
-        await client.join_chat("i_m_fighter")
+        await client.join_chat("sagebotxsupports")
     except BaseException:
         pass
